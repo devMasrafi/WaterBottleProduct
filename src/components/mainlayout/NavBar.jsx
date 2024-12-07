6beb0e8898618b40b02266a5cbce3af230211307
Md. Masrafi Mondol <146985678+devMasrafi@users.noreply.github.com>
2024-12-07 20:37:34 +0600

    update navbar

--- src/components/mainlayout/NavBar.jsx
+++ src/components/mainlayout/NavBar.jsx
@@ -4,12 +4,12 @@ import { GiHamburgerMenu } from "react-icons/gi";
 const NavBar = () => {
   return (
     <nav className='sticky top-0 z-50'>
-        <div className='container mx-auto flex justify-between text-white  '>
+        <div className='container mx-auto flex justify-between text-white pt-2  '>
             <div>
-                <h1>Logo</h1>
+                <h1 className='text-2xl font-bold'>Logo</h1>
             </div>
             <div>
-                <ul className='flex gap-x-4 '>
+                <ul className='flex gap-x-4 font-raleWayFont capitalize tracking-wide '>
                     <li>Home</li>
                     <li>About</li>
                     <li>Contact</li>
